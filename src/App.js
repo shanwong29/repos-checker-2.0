2a71920bba5ada0b00687d23203864f55125e637
Shan Wong <shan.wong29@gmail.com>
2020-03-19 23:13:58 +0100

    add github-token login function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import Login from "./Component/Login/Login";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const accessToken = localStorage.getItem("token");
+
+const httpLink = new HttpLink({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${accessToken}`
+  }
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+const App = props => {
+  return (
+    <>
+      {accessToken ? (
+        <ApolloProvider client={client}>
+          <div className="App">
+            <h1>Repos Checker</h1>
+
+            <button
+              onClick={() => {
+                localStorage.clear();
+                window.location.reload();
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </ApolloProvider>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
+};
 
 export default App;
---
+++ src/Component/Login/Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [token, setToken] = useState("");
+
+  const tokenSubmit = e => {
+    e.preventDefault();
+    localStorage.setItem("token", token);
+    window.location.reload();
+  };
+
+  return (
+    <section>
+      <form onSubmit={tokenSubmit}>
+        <input
+          type="password"
+          name="token"
+          value={token}
+          onChange={e => {
+            setToken(e.target.value);
+          }}
+          placeholder="Paste your GitHub token"
+        />
+        <button>SUBMIT</button>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
