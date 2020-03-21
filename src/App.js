bc8457946ef5c8def8ab87f162bdf22a8c04555d
Shan Wong <shan.wong29@gmail.com>
2020-03-21 17:34:50 +0100

    move Apollo setup to index.js for better structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+
 import ReposInfo from "./Component/ReposInfo/ReposInfo";
 import SearchField from "./Component/SearchField/SearchField";
 import Login from "./Component/Login/Login";
@@ -10,18 +7,6 @@ import "./App.css";
 
 const App = props => {
   const accessToken = localStorage.getItem("token");
-
-  const httpLink = new HttpLink({
-    uri: "https://api.github.com/graphql",
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  });
-
-  const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-  });
 
   const [reposQuery, setReposQuery] = useState("");
 
@@ -38,21 +23,19 @@ const App = props => {
   return (
     <>
       {accessToken ? (
-        <ApolloProvider client={client}>
-          <div className="App">
-            <button className="" onClick={logout}>
-              Logout
-            </button>
-            <h1>Repos Checker</h1>
-            <SearchField
-              reposQuery={reposQuery}
-              setReposQuery={setReposQuery}
-              handleSubmit={handleSubmit}
-            />
+        <div className="App">
+          <button className="" onClick={logout}>
+            Logout
+          </button>
+          <h1>Repos Checker</h1>
+          <SearchField
+            reposQuery={reposQuery}
+            setReposQuery={setReposQuery}
+            handleSubmit={handleSubmit}
+          />
 
-            <ReposInfo />
-          </div>
-        </ApolloProvider>
+          <ReposInfo />
+        </div>
       ) : (
         <Login />
       )}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const accessToken = localStorage.getItem("token");
+
+const httpLink = new HttpLink({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${accessToken}`
+  }
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
