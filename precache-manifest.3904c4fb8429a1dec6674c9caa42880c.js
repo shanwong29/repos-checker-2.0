64ad50fa397739acddfaceeee675aa778779eb07
shanwong29 <shanwong29@users.noreply.github.com>
2020-06-17 12:54:59 +0000

    deploy: 65536077443ba4de4ac41f024d36728cc36f1f23

---
+++ precache-manifest.3904c4fb8429a1dec6674c9caa42880c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab703d19c037565f00289c59f95a120",
+    "url": "/repos-checker-2.0/index.html"
+  },
+  {
+    "revision": "bba3032d19530e39fc76",
+    "url": "/repos-checker-2.0/static/css/main.e094a29f.chunk.css"
+  },
+  {
+    "revision": "6281b24635c59da0c253",
+    "url": "/repos-checker-2.0/static/js/2.690188fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repos-checker-2.0/static/js/2.690188fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba3032d19530e39fc76",
+    "url": "/repos-checker-2.0/static/js/main.d07ea196.chunk.js"
+  },
+  {
+    "revision": "9dc734dfe956df67caec",
+    "url": "/repos-checker-2.0/static/js/runtime-main.a87f64a8.js"
+  }
+]);
